37c2b3175d796f193a14c84a7849f6af232966b1
vishakagunasekera <vishaka1602@gmail.com>
2016-11-18 16:08:18 -0500

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      //create student named Karlie
+      Student karlie = new Student("Karlie Kloss", 113587313, 3.2, 4);
+
+      //have karlie take a test
+      karlie.takeTest();
+   }
+}
